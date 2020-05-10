7f88dbddb996a76d0b62fc00aa76cabe97ef6f9f
ParadoxInfinite <samarthkulkarnis@yahoo.co.in>
2020-05-10 22:51:52 +0530

    Fixed register and lot of other stuff

--- FrontEndCode/app.js
+++ FrontEndCode/app.js
@@ -1,36 +1,41 @@
 // Requiring packages that will be used.
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
-const path = require('path');
-const indexRoutes = require('./routes/index');
-const authRoutes = require('./routes/authentication');
-const registerRoutes = require('./routes/register');
-const adminRoutes = require('./routes/adminpanel');
-const session = require('express-session');
+const path = require("path");
+const indexRoutes = require("./routes/index");
+const authRoutes = require("./routes/authentication");
+const registerRoutes = require("./routes/register");
+const adminRoutes = require("./routes/adminpanel");
+const session = require("express-session");
 
-app.use(session({secret: 'mySecret', resave: false, saveUninitialized: false})); // Setting the session and secret for the context to be passed.
+app.use(
+  session({ secret: "mySecret", resave: false, saveUninitialized: false })
+); // Setting the session and secret for the context to be passed.
 
-app.set('view engine', 'ejs'); // Setting the view engine to ejs so it renders the ejs files. 
+app.set("view engine", "ejs"); // Setting the view engine to ejs so it renders the ejs files.
 
 app.use(bodyParser.json()); // BodyParser to retrieve data from req.body.
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-  
-app.use(express.static(path.join(__dirname, 'public'))) // Declaring the public directory to limit exposure to the server.
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public"))); // Declaring the public directory to limit exposure to the server.
 
 // Connecting all the routes to the main app.js
-app.use(indexRoutes)
-app.use(authRoutes)
-app.use(registerRoutes)
-app.use(adminRoutes)
+app.use(indexRoutes);
+app.use(authRoutes);
+app.use(registerRoutes);
+app.use(adminRoutes);
 
-app.get('/', (req, res, next) => { // Redirecting the requests/hits on '/' to '/index'
-    res.redirect('/index')
-    next();
-  })
+app.get("/", (req, res, next) => {
+  // Redirecting the requests/hits on '/' to '/index'
+  res.redirect("/index");
+  next();
+});
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')) // If there is an attempt to access invalid URL, 404 page is displayed.
-  })
+  res.status(404).render("404"); // If there is an attempt to access invalid URL, 404 page is displayed.
+});
 app.listen(5000); //Listening on port 5000 (localhost:5000). Can be changed later.
--- FrontEndCode/routes/register.js
+++ FrontEndCode/routes/register.js
@@ -1,67 +1,83 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
 const router = express.Router();
 
 // Variables to generate account number and customer id in register page. (This is temp, need to find a fix.)
-var accno = 1000001
-var customerid = 99999001
+var accno = 1000001;
+var customerid = 99999001;
 
-function CaptureFinger(quality, timeout) { // Same as authentication.js, refer there for info
+function CaptureFinger(quality, timeout) {
+  // Same as authentication.js, refer there for info
   var MFS100Request = {
-      "Quality": quality,
-      "TimeOut": timeout
+    Quality: quality,
+    TimeOut: timeout,
   };
   var jsonData = JSON.stringify(MFS100Request);
   return PostMFS100Client("capture", jsonData);
 }
 
-function PostMFS100Client(method, jsonData) { // Same as authentication.js, refer there for info
+function PostMFS100Client(method, jsonData) {
+  // Same as authentication.js, refer there for info
   var res;
   $.support.cors = true;
   var httpStatus = false;
   // Ajax request to match the fingerprint
   $.ajax({
-      type: "POST",
-      async: false,
-      crossDomain: true,
-      url: uri + method,
-      contentType: "application/json; charset=utf-8",
-      data: jsonData,
-      dataType: "json",
-      processData: false,
-      success: function (data) { // Enters this if the request was answered, might not be a match still.
-          httpStatus = true;
-          res = { httpStatus: httpStatus, data: data };
-      },
-      error: function (jqXHR, ajaxOptions, thrownError) { // If the Ajax request fails, it enters this code.
-          console.log("Ajaxops:" + ajaxOptions + " errthrown : " + thrownError)
-          res = { httpStatus: httpStatus, err: getHttpError(jqXHR) };
-      },
+    type: "POST",
+    async: false,
+    crossDomain: true,
+    url: uri + method,
+    contentType: "application/json; charset=utf-8",
+    data: jsonData,
+    dataType: "json",
+    processData: false,
+    success: function (data) {
+      // Enters this if the request was answered, might not be a match still.
+      httpStatus = true;
+      res = { httpStatus: httpStatus, data: data };
+    },
+    error: function (jqXHR, ajaxOptions, thrownError) {
+      // If the Ajax request fails, it enters this code.
+      console.log("Ajaxops:" + ajaxOptions + " errthrown : " + thrownError);
+      res = { httpStatus: httpStatus, err: getHttpError(jqXHR) };
+    },
   });
   return res;
 }
 
 // GET request to /register handled here.
-router.get('/register', (req, res, next) => { // Increment the customerid and accno to render. (Valid only in this session.)
-  customerid+=1;
-  accno+=1;
-  res.render('register',{ // Render the page with incremented data
-    'cardno' : req.session.context,
-    'customerid' : JSON.stringify(customerid),
-    'accno' : JSON.stringify(accno)
-  });
+router.get("/register", (req, res, next) => {
+  // Increment the customerid and accno to render. (Valid only in this session.)
+  customerid += 2;
+  accno += 2;
+  if (typeof req.session.context == "string") {
+    res.render("register", {
+      // Render the page with incremented data
+      cardno: req.session.context,
+      customerid: JSON.stringify(customerid),
+      accno: JSON.stringify(accno),
+    });
+  } else {
+    res.status(401).render("401");
+  }
 });
 
-router.post('/register', (req,res,next) => {
-  MongoClient.connect('mongodb://localhost:27017/bankdb', { // Connecting to our database (bankdb) on MongoDB
-    useNewUrlParser: true
-  }, function(err, client) { //Callback function for the connection
-    if (err) throw err // If the connection fails, throws err
-    var db = client.db('bankdb') // Var for ease of use and also traditional Mongo usage.
-    db.collection('users').insertOne(req.body); // req.body contains the exact JSON we need as a query to insert.
-  })
-  res.redirect('index'); // Redirect to index page after registration.
+router.post("/register", (req, res) => {
+  MongoClient.connect(
+    "mongodb://localhost:27017/bankdb",
+    {
+      // Connecting to our database (bankdb) on MongoDB
+      useNewUrlParser: true,
+    },
+    function (err, client) {
+      //Callback function for the connection
+      if (err) throw err; // If the connection fails, throws err
+      var db = client.db("bankdb"); // Var for ease of use and also traditional Mongo usage.
+      db.collection("users").insertOne(req.body); // req.body contains the exact JSON we need as a query to insert.
+    }
+  );
+  res.render("registersuccess"); // Redirect to index page after registration.
 });
 
 module.exports = router;
